976527940e7fd2fa8875d90ef6d0881d47c358c0
feldblume5263 <feldblume5263@gmail.com>
2022-03-03 23:26:40 +0900

    update code

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,16 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var body: some View{
-        ZStack(content: {
-            RoundedRectangle(cornerRadius: 25.0)
-                .stroke(lineWidth: 3)
-                .foregroundColor(.blue)
-            Text("Hello World")
-                .padding(.all)
-        })
-        .padding(5)
-        .ignoresSafeArea()
-        
+    var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    var content: String = ""
+    @State var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️")
+                    .font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
