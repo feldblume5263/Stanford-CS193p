10854397b89a43887eebf34dc25573418af50f2a
feldblume5263 <feldblume5263@gmail.com>
2022-03-07 14:57:26 +0900

    update lecture 2 code

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,22 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚂", "🚀", "🚁", "🚜", "🚗", "🚄", "🚤", "🚲", "🚌", "🚑", "🚟", "🚔", "🛵", "🚕", "🚒", "🛴", "✈️", "🛸", "🚠", "🚚", "🏎", "⛵️", "🏍", "🚢"]
+    @State var emojiCount = 4
     
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(.red)
     }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            VStack {
+                Image(systemName: "minus.circle")
+            }
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            VStack {
+                Image(systemName: "plus.circle")
+            }
+        }
+    }
 }
 
 struct CardView: View {
-    var content: String = ""
-    @State var isFaceUp: Bool = false
+    var content: String
+    @State var isFaceUp: Bool = true
     
     var body: some View {
         ZStack {
@@ -31,7 +66,7 @@ struct CardView: View {
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
                 shape.stroke(lineWidth: 3)
-                Text("✈️")
+                Text(content)
                     .font(.largeTitle)
             } else {
                 shape.fill()
@@ -42,6 +77,9 @@ struct CardView: View {
         }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
