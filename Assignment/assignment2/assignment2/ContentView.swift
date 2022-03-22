a91ab871843c6451958cd5cdafd7f8dd923d1c36
feldblume5263 <feldblume5263@gmail.com>
2022-03-22 23:02:54 +0900

    start assignment2

---
+++ Assignment/assignment2/assignment2/ContentView.swift
@@ -0,0 +1,99 @@
+//
+//  ContentView.swift
+//  assignment2
+//
+//  Created by Noah Park on 2022/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let vehicles = ["🚂", "🚜", "🚗", "🚄", "🚌", "🚑", "🚔", "🛵", "🚕", "🚒", "🚚", "🏍"]
+    let fruits = ["🍎", "🍋", "🍉", "🍇", "🍓", "🫐", "🍈", "🍒", "🍑", "🥭", "🍍", "🥝"]
+    let flags =  ["🇰🇷", "🇩🇪", "🇺🇦", "🇺🇸", "🇫🇷", "🇪🇸", "🇰🇷", "🇨🇦", "🇰🇭", "🇮🇶", "🇬🇧", "🇮🇩"]
+    
+    @State var emogis = ["🚂", "🚜", "🚗", "🚄", "🚌", "🚑", "🚔", "🛵", "🚕", "🚒", "🚚",  "🏍"]
+    
+    var body: some View {
+        VStack {
+            Text("Memorize!")
+                .font(.largeTitle)
+                .padding()
+            Spacer()
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80.0))]) {
+                    ForEach(emogis.indices, id: \.self) { idx in
+                        CardView(content: emogis[idx])
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                ThemeButton(emojis: $emogis, themeIcon: "🚗", themeName: "Vehicles", newTheme: vehicles)
+                Spacer()
+                ThemeButton(emojis: $emogis, themeIcon: "🍎", themeName: "Fruits", newTheme: fruits)
+                Spacer()
+                ThemeButton(emojis: $emogis, themeIcon: "🇰🇷", themeName: "Flags", newTheme: flags)
+                Spacer()
+            }
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct ThemeButton: View {
+    @Binding var emojis: [String]
+    var themeIcon: String
+    var themeName: String
+    var newTheme: [String]
+    
+    var body: some View {
+        VStack {
+            Text(themeIcon)
+                .font(.largeTitle)
+            Text(themeName)
+                .font(.caption)
+        }
+        .onTapGesture {
+            emojis = newTheme.shuffled()
+        }
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    let cardShape = RoundedRectangle(cornerRadius: 20)
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp == true {
+                cardShape
+                    .fill().foregroundColor(.white)
+                cardShape
+                    .strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                cardShape
+                    .fill()
+            }
+        }
+        .foregroundColor(.blue)
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewInterfaceOrientation(.portrait)
+    }
+}
+
+
---
+++ Assignment/assignment2/assignment2/assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment2App.swift
+//  assignment2
+//
+//  Created by Noah Park on 2022/03/09.
+//
+
+import SwiftUI
+
+@main
+struct assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
